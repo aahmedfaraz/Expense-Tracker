63725472e142145f84116949bf79bd67537ab44a
Ahmed Faraz <ahmedfaraz.business@gmail.com>
2021-04-12 19:30:29 +0500

    About updated

--- expense-tracker/src/Components/pages/About.js
+++ expense-tracker/src/Components/pages/About.js
@@ -3,7 +3,9 @@ import React, { Fragment } from 'react'
 const About = () => {
     return (
         <Fragment>
-            <p className="about">Developed by <span>Ahmed Faraz.</span> <br/>It is a react application <span>Expense Tracker</span> which allows you to <br/>easily manage your expenses.</p>
+            <h1 style={{padding:"0 20px"}}>About the Expense Tracker App</h1>
+            <p className="about">Developed by<br/><span>Ahmed Faraz</span></p>
+            <p className="about">This is a react application<br/><span>Expense Tracker</span><br/>which allows<br/>easy management of your expenses.</p>
         </Fragment>
     )
 }
